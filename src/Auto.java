2f4987b9bf081bcd4733fa46deb6a7a049b35fa5
USRVI-LC2 <USRVI-LC2@VIAWSE02LC0200.duoc.acad>
2024-08-28 14:34:35 -0400

    probando git

---
+++ src/Auto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Profe Hernán
+ * @version 28-08-2024
+ */
+public class Auto {
+    
+    private String marca,modelo,color,tipoTransmision,tipoCombustible;
+    private int año,cantidadPuertas;
+    private double cilindrada;
+    
+    
+    
+    
+}
